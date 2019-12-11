12cff6dd52216b14f55fb8c820dfb85418e7c319
Rafay Khawar <rafaykhawar09@gmail.com>
2019-12-10 16:34:45 -0800

    updating css and adding background-color according to the current time

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
 var time = ["9 AM", "10 AM", "11 AM", "12 AM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
 
+var numTime = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+const m = moment();
+$("#currentDay").text((m.format("dddd MMM Mo YYYY")));
+var currentTime = parseInt(m.format("HH"));
+
 //create elements    
 for (let i = 0; i < time.length; i++) {
           
@@ -15,9 +21,9 @@ for (let i = 0; i < time.length; i++) {
 	row.attr("class", "row");
 
 	row.append(timeblock);
-	timeblock.attr("class", "timeblock");
+	timeblock.attr("class", "time-block");
 	timeblock.append(hour);
-	hour.attr("class", "hour description");
+	hour.attr("class", "hour");
 	hour.text(time[i]);
 
 	row.append(textarea);
@@ -25,10 +31,14 @@ for (let i = 0; i < time.length; i++) {
 	row.append(saveBtn);
 	saveBtn.attr("class", "saveBtn");
 	saveBtn.text("Save!");
+
+	if(numTime[i] < currentTime){
+		textarea.attr("class", "past");
+	}
+	else if(numTime[i] === currentTime){
+		textarea.attr("class", "present");
+	}
+	else if(numTime[i] > currentTime){
+		textarea.attr("class", "future");
+	}
 }
-
-
-
-const m = moment();
-$("#currentDay").text((m.format("dddd MMM Mo YYYY")));
-console.log("Time: " + m.format("h A"));
